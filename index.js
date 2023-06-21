2eb9e08708bb91d622d76e326c95bd5a2393d52a
taejin Kim <rlaxowls1316@naver.com>
2023-06-21 11:40:09 +0900

    Add ball moving

--- index.js
+++ index.js
@@ -5,26 +5,39 @@ canvas.width = 600;
 canvas.height = 400;
 
 const ballRadius = 11;
+let ballSpeed = 4;
 let x = canvas.width / 2;
-let y = canvas.height;
+let y = canvas.height - ballRadius;
 
-function circle() {
+let dx = ballSpeed;
+let dy = ballSpeed;
+
+function circle(x, y) {
+  ctx.beginPath();
   ctx.fillStyle = "royalblue";
-  ctx.arc(
-    canvas.width / 2,
-    canvas.height - ballRadius,
-    ballRadius,
-    0,
-    Math.PI * 2
-  );
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fill();
+  ctx.closePath();
+}
+
+function checkCorner() {
+  if (x - ballRadius < 0 || x + ballRadius > canvas.width) {
+    dx *= -1;
+  }
+  if (y - ballRadius < 0 || y + ballRadius > canvas.height) {
+    dy *= -1;
+  }
 }
 
 function animate() {
   ctx.fillStyle = "#dce0e3";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  circle();
+  circle(x, y);
+  checkCorner();
+
+  x -= dx;
+  y -= dy;
   requestAnimationFrame(animate);
 }
 
