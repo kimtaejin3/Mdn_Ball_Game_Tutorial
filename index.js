9ac49f8253236163b626815383bc75ad2af69091
taejin Kim <rlaxowls1316@naver.com>
2023-06-21 11:27:28 +0900

    create ball without moving

---
+++ index.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 400;
+
+const ballRadius = 11;
+
+function circle() {
+  ctx.fillStyle = "royalblue";
+  ctx.arc(
+    canvas.width / 2,
+    canvas.height - ballRadius,
+    ballRadius,
+    0,
+    Math.PI * 2
+  );
+  ctx.fill();
+}
+
+function animate() {
+  ctx.fillStyle = "#dce0e3";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  circle();
+  requestAnimationFrame(animate);
+}
+
+animate();
